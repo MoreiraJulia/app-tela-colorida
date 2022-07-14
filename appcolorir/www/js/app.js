36ae5375b983c29e9ad24a94fda660a6c5f8b871
Júlia Aparecida Moreira <julia.aparecida25@gmail.com>
2022-07-14 20:34:54 -0300

    Atualizações

--- appcolorir/www/js/app.js
+++ appcolorir/www/js/app.js
@@ -1,7 +1,7 @@
  function MudarFundo(){
      let corR = Math.floor(Math.random() * 255)
      let corG = Math.floor(Math.random() * 255)
-     let corB = Math.floor(Math.random() * 255 *(max-min+1)+min)
+     let corB = Math.floor(Math.random() * 255)
      let cor = `rgb(${corR} , ${corG}, ${corB})`
     console.log(cor)
      document.body.style.backgroundColor = cor
